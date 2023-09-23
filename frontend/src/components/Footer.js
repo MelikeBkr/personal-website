ae1058b8e1da86f9db19c21e5d448b9db7a86c2b
Melike Bakır <39765068+MelikeBkr@users.noreply.github.com>
2023-09-23 15:40:12 +0100

    workaround solution is added for App.css This issue will be fixed permanently

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Footer() {
     return (
         <footer>
-            <p>© 2023 Your Name. All rights reserved.</p>
+            <p>© 2023 Melike Bakir. All rights reserved.</p>
         </footer>
     );
 }
